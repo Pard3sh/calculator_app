957db11b5b5ac72138e1f06b1d9a9f081a69a78a
pard3sh <pard3sh@bu.edu>
2025-11-13 19:10:12 -0500

    learned how to draw a basic window with an entry and a frame

--- scratch/draw_window.py
+++ scratch/draw_window.py
@@ -1,6 +1,19 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.geometry("500x500")
-root.title("my first")
+root.geometry('500x500')
+root.title('my first')
+
+label = tk.Label(root, text='Hello World', font=('Arial', 18))
+label.pack(padx=20, pady=20)
+
+textbox = tk.Text(root, font=('Arial', 16))
+textbox.pack(padx=10)
+
+button_frame = tk.Frame(root)
+button_frame.columnconfigure(0, weight=1)
+
+button = tk.Button(root, text='Submit', font=('Arial', 16))
+button.pack(padx=10, pady=10)
+
 root.mainloop()
